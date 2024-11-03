b68ef9ac71d5b3dc1c0dbc774904231a98084964
cinarkilci <cinarkilci@sabanciuniv.edu>
2024-11-03 22:28:50 +0300

    create product type

---
+++ frontend/types/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: number;
+  serialNumber: string;
+  name: string;
+  description: string;
+  category: string;
+  subCategory: string;
+  price: number;
+  quantity: number;
+  image: string;
+  warrantyStatus: string;
+};
