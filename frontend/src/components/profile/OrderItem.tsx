35e367362509379ca92993a8a90490b8e5dd8240
cinarkilci <cinarkilci@sabanciuniv.edu>
2024-12-29 00:25:30 +0300

    Update order item

--- frontend/src/components/profile/OrderItem.tsx
+++ frontend/src/components/profile/OrderItem.tsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaChevronDown } from "react-icons/fa6";
 import { FaChevronUp } from "react-icons/fa";
 import ReviewCard from "@/components/cards/ReviewCard";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { requestRefund, cancelOrder, getOrderDetails } from "@/lib/requests";
+import { useMutation, QueryClient, useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+const queryClient = new QueryClient();
 
 export default function OrderItem({ item }: { item: any }) {
   const [expanded, setExpanded] = useState(false);
+  const canRefund =
+    item.order_status === "Delivered" || item.order_status === "In-Transit";
+  const canCancel =
+    item.order_status === "In-Transit" || item.order_status === "Processing";
+
+  const { mutate: cancelOrderMutation, isPending } = useMutation({
+    mutationFn: async () => cancelOrder(item.order_id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
+      toast.success("Order canceled");
+    },
+  });
 
   return (
     <div className="flex flex-col divide-y rounded-lg border border-gray-200 bg-white shadow-sm min-h-[100px]">
@@ -54,6 +72,18 @@ export default function OrderItem({ item }: { item: any }) {
           </div>
         </div>
 
+        {canCancel && (
+          <div className="flex-1 text-center">
+            <Button
+              className="bg-red-500 text-white px-4 py-2 rounded-md"
+              onClick={() => cancelOrderMutation()}
+              disabled={isPending}
+            >
+              Cancel Order
+            </Button>
+          </div>
+        )}
+
         {!expanded ? <FaChevronDown /> : <FaChevronUp />}
       </div>
 
@@ -91,6 +121,13 @@ export default function OrderItem({ item }: { item: any }) {
                     <ReviewCard product={orderItem} />
                   </div>
                 )}
+                {canRefund && (
+                  <RefundSection
+                    orderId={item.order_id}
+                    productId={orderItem.product_id}
+                    totalQuantity={orderItem.quantity}
+                  />
+                )}
               </div>
             ))}
           </div>
@@ -99,3 +136,87 @@ export default function OrderItem({ item }: { item: any }) {
     </div>
   );
 }
+
+const RefundSection = ({
+  orderId,
+  productId,
+  totalQuantity,
+}: {
+  orderId: number;
+  productId: number;
+  totalQuantity: number;
+}) => {
+  const [refundQuantity, setRefundQuantity] = useState(totalQuantity);
+  const { data: orderDetails } = useQuery({
+    queryKey: ["orderDetails", orderId],
+    queryFn: () => getOrderDetails(orderId),
+  });
+  const [orderItemStatus, setOrderItemStatus] = useState(
+    orderDetails?.order?.orderItems.find(
+      (item: any) => item.product_id === productId
+    ).item_status
+  );
+
+  console.log(orderDetails);
+
+  useEffect(() => {
+    setOrderItemStatus(
+      orderDetails?.order?.orderItems.find(
+        (item: any) => item.product_id === productId
+      ).item_status
+    );
+  }, [orderDetails]);
+
+  const { mutate: requestRefundMutation } = useMutation({
+    mutationFn: async ({
+      orderId,
+      productId,
+      quantity,
+    }: {
+      orderId: number;
+      productId: number;
+      quantity: number;
+    }) => {
+      const orderItemId = orderDetails?.order?.orderItems.find(
+        (item: any) => item.product_id === productId
+      ).order_item_id;
+      requestRefund(orderId, orderItemId, quantity);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
+      toast.success("Refund request submitted");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return (
+    <div className="flex flex-col justify-center gap-2">
+      {orderItemStatus ? (
+        <p>{orderItemStatus}</p>
+      ) : (
+        <>
+          <Button
+            className="bg-red-500 text-white px-4 py-2 rounded-md"
+            onClick={() =>
+              requestRefundMutation({
+                orderId,
+                productId,
+                quantity: refundQuantity,
+              })
+            }
+          >
+            Request Return
+          </Button>
+          <Input
+            type="number"
+            placeholder="Quantity"
+            defaultValue={refundQuantity}
+            onChange={(e) => setRefundQuantity(Number(e.target.value))}
+          />
+        </>
+      )}
+    </div>
+  );
+};
